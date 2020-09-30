1ada0a68ebe3e540aa2e05f506ccc0aca99fa9b7
swmetcode <72120488+swmetcode@users.noreply.github.com>
2020-09-30 08:06:34 +0300

    tst.sh

--- tst.sh
+++ tst.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #!/bin/sh
 #Code:Samet Polat
-#Türk Siber Timi
-#İnstagram : @turksibertimi
-#Youtube : YamanEfkar
+#swmetcode
+#İnstagram : @swmet.wq
 clear
 case $1 in
 
@@ -63,9 +62,9 @@ echo -e '\033[31;40;1m
 	────────────────█████████───────────────
 	──────────────█████████████─────────────
 
-	   \e[35m☾★ ☾★ ☾★ ☾★ YamanEfkar ☾★ ☾★ ☾★ ☾★	
-	 \e[31m☾★ ☾★ ☾★ ☾★ Türk Siber Timi ☾★ ☾★ ☾★ ☾★	
-  \e[32m☾★ ☾★ ☾★ ☾★ İnstagram : @turksibertimi ☾★ ☾★ ☾★ ☾★	
+	   \e[35m☾★ ☾★ ☾★ ☾★ SametPolat ☾★ ☾★ ☾★ ☾★	
+	 \e[31m☾★ ☾★ ☾★ ☾★ Swmet Code Hacking ☾★ ☾★ ☾★ ☾★	
+  \e[32m☾★ ☾★ ☾★ ☾★ İnstagram : @swmetcode ☾★ ☾★ ☾★ ☾★	
 '
 echo -e '
 \e[31m[01]\e[32mİnstagram			\e[31m[04]\e[32mFacebook
